01c7cad24397f4f50f4c92031d496d36c7ff8c5b
mosajjid <mosajjid.decrypt@gmail.com>
2023-01-13 11:24:49 +0530

    bugs remove

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,8 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  
+  windowScrolled: boolean;
 
   constructor() { }
+  
+  @HostListener("window:scroll", [])
+    onWindowScroll() {
+        if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+            this.windowScrolled = true;
+        } 
+       else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+            this.windowScrolled = false;
+        }
+    }
+    scrollToTop() {
+      window.scrollTo(0, 0)
+    }
 
   ngOnInit(): void {
   }
